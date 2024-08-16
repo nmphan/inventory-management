b7534af23e1d577b93cae241db61f41c7c7ee71c
nmphan <nmphan205@gmail.com>
2024-08-16 13:15:25 -0600

    update

---
+++ Data/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Data
+{
+    public class Category
+    {
+        int categoryId;
+        string CategoryName;
+
+        public int CategoryId { get => categoryId; set => categoryId = value; }
+        public string CategoryName1 { get => CategoryName; set => CategoryName = value; }
+
+        public Category()
+        {
+            
+        }
+
+        public Category(int categoryId, string categoryName)
+        {
+            this.categoryId = categoryId;
+            CategoryName = categoryName;
+        }
+    }
+}
